644f9350b3fb0f7e80ff44f5bdce59ce73deaad5
Vanne <cn.t0604@gmail.com>
2024-05-09 20:47:11 -0500

    h

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify, redirect, url_for
 import mysql.connector
+
 app = Flask(__name__)
 
 db = mysql.connector.connect(
@@ -9,24 +10,75 @@ db = mysql.connector.connect(
     database = "AGLAIA"
 
 )
-db.close()
 
-
-
+suggestions_data = ["Python", "Flask", "JavaScript", "HTML", "CSS", "React", "Vue.js", "Django", "Node.js"]
 
 @app.route('/')
 def index():
     return render_template ('index.html')
 
 
+@app.route('/Login')
+def login():
+    return render_template ('General/Login.html')
 
+@app.route('/form')
+def form():
+    return render_template('Emprendedor/Formulario.html')
 
 @app.route('/AP')
 def AprobarP():
     return render_template ('Administrador/AprobarP.html')
 
+@app.route('/Editor')
+def editor():
+    return render_template('Emprendedor/Editor.html')
+
+
+
+
+@app.route('/registro_usuario', methods=['GET', 'POST'])
+def registro_usuario():
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        apellido = request.form['apellido']
+        email = request.form['email']
+        contraseña = request.form['contraseña']
+      
+        cur = db.cursor()
+        cur.execute("INSERT INTO Usuarios (nombre, apellido, email, contraseña) VALUES (%s, %s, %s, %s)", (nombre, apellido, email, contraseña))
+        db.commit()
+        cur.close()
+        return redirect(url_for('index'))
+    return render_template('Registro.html')
+
+
+@app.route('/iniciar_sesion', methods=['POST'])
+def iniciar_sesion():
+    if request.method == 'POST':
+        email = request.form['email']
+        contraseña = request.form['contraseña']
+        cur = db.cursor()
+       
+        result = cur.execute("SELECT * FROM Usuarios WHERE email = %s AND contraseña = %s", (email, contraseña))
+        if result > 0:
+            
+            return redirect(url_for('index'))
+        else:
+          
+            return redirect(url_for('error_inicio_sesion'))
+    # return render_template('Login.html')
+
+
+@app.route('/error_inicio_sesion')
+def error_inicio_sesion():
+    return render_template('error_inicio_sesion.html')
+
+@app.route("/autocomplete")
+def autocomplete():
+    query = request.args.get("q", "")
+    results = [item for item in suggestions_data if query.lower() in item.lower()]
+    return jsonify(results)
 
 if __name__ == '__main__':
-
-    app.add_url_rule('/', view_func=index)
-    app.run(debug=True,port=5000)
+    app.run(debug=True)
--- app/capp.py
+++ app/capp.py
@@ -1,66 +1,32 @@
-from flask import Flask, render_template, request, jsonify, redirect, url_for
-from flask_mysqldb import MySQL
-
+from flask import Flask, render_template
+import mysql.connector
 app = Flask(__name__)
 
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'usuario_mysql'
-app.config['MYSQL_PASSWORD'] = 'contraseña_mysql'
-app.config['MYSQL_DB'] = 'AGLAIA'
-app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
-mysql = MySQL(app)
+db = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password="",
+    database = "AGLAIA"
 
-suggestions_data = ["Python", "Flask", "JavaScript", "HTML", "CSS", "React", "Vue.js", "Django", "Node.js"]
+)
+db.close()
+
+
 
 
 @app.route('/')
 def index():
-    return render_template('inicio_sesion.html')
+    return render_template ('index.html')
 
 
-@app.route('/registro_usuario', methods=['GET', 'POST'])
-def registro_usuario():
-    if request.method == 'POST':
-        nombre = request.form['nombre']
-        apellido = request.form['apellido']
-        email = request.form['email']
-        contraseña = request.form['contraseña']
-      
-        cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO Usuarios (nombre, apellido, email, contraseña) VALUES (%s, %s, %s, %s)", (nombre, apellido, email, contraseña))
-        mysql.connection.commit()
-        cur.close()
-        return redirect(url_for('index'))
-    return render_template('registro_usuario.html')
 
 
-@app.route('/iniciar_sesion', methods=['POST'])
-def iniciar_sesion():
-    if request.method == 'POST':
-        email = request.form['email']
-        contraseña = request.form['contraseña']
-       
-        cur = mysql.connection.cursor()
-        result = cur.execute("SELECT * FROM Usuarios WHERE email = %s AND contraseña = %s", (email, contraseña))
-        if result > 0:
-            
-            return redirect(url_for('index'))
-        else:
-          
-            return redirect(url_for('error_inicio_sesion'))
-    return render_template('inicio_sesion.html')
+@app.route('/AP')
+def AprobarP():
+    return render_template ('Administrador/AprobarP.html')
 
-
-@app.route('/error_inicio_sesion')
-def error_inicio_sesion():
-    return render_template('error_inicio_sesion.html')
-
-@app.route("/autocomplete")
-def autocomplete():
-    query = request.args.get("q", "")
-    results = [item for item in suggestions_data if query.lower() in item.lower()]
-    return jsonify(results)
 
 if __name__ == '__main__':
-    app.run(debug=True)
-=======
+
+    app.add_url_rule('/', view_func=index)
+    app.run(debug=True,port=5000)
